ffcfcbb3d237a63e08ef001c67bdfdcf4fc890cc
Kate Ross <kaeross@icloud.com>
2017-08-10 20:03:34 +0100

    Update 6
    Successfully retrieves and writes contacts to user cards

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-
 const $userCards = $('.user');
 let newPerson;
 
-//Get new contact data from random user generator api
-//function getContact(card) {
-	$.ajax({
-		url: 'https://randomuser.me/api/?results=12',
-		dataType: 'json',
-		//if successful store relevent data about user
-		success: function(data) {
-			console.log(data);
-			newPerson = data;
-			return newPerson;			
-		}
-	});
-
-
-//}
-
 //function to get data and store in object
-function getPersonData() {
-	const personResults = newPerson.results[0];
+function getPersonData(i) {
+	const personResults = newPerson.results[i];
 	const personData = {
 		name: personResults.name.first + ' ' + personResults.name.last,
 		email: personResults.email,
@@ -32,41 +15,39 @@ function getPersonData() {
 	return personData;
 }
 
-//create person
-function Person(name, email, city, phone, avatar, address){
-	this.name = name;
-	this.email = email;
-	this.city = city;
-	this.phone = phone;
-	this.avatar = avatar;
-	this.address = address;
-};
-
 //write contact to user card
-Person.prototype.writeUserCard = function(card){
-	const $avatarHTML = $('> .avatar', card);
-	const $phoneHTML = $('> div > .phone', card);
-	const $cityHTML = $('> div > .location', card);
-	const $addressHTML = $('> div > .address', card);
-	const $emailHTML = $('> div > .email', card);
-	const $nameHTML = $('> div > .name', card);
+function writeUserCard(num){
+	const $avatarHTML = $('> .avatar', $userCards[num]);
+	const $phoneHTML = $('> div > .phone', $userCards[num]);
+	const $cityHTML = $('> div > .location', $userCards[num]);
+	const $addressHTML = $('> div > .address', $userCards[num]);
+	const $emailHTML = $('> div > .email', $userCards[num]);
+	const $nameHTML = $('> div > .name', $userCards[num]);
 
-	$avatarHTML.attr('src', this.avatar);
-	$phoneHTML.html(this.phone);
-	$cityHTML.html(this.city);
-	$addressHTML.html(this.address);
-	$emailHTML.html(this.email);
-	$nameHTML.html(this.name);
+	$avatarHTML.attr('src', getPersonData(num).avatar);
+	$phoneHTML.html(getPersonData(num).phone);
+	$cityHTML.html(getPersonData(num).city);
+	$addressHTML.html(getPersonData(num).address);
+	$emailHTML.html(getPersonData(num).email);
+	$nameHTML.html(getPersonData(num).name);
 };
-//create newPerson and add content to user card
-var getPerson = new Person(getPersonData().name, getPersonData().email, getPersonData().city, getPersonData().phone, getPersonData().avatar, getPersonData().address)
-getPerson.writeUserCard(card);
 
+//writes each user to each card
 function writeEachCard() {
 	for (let i = 0; i < $userCards.length; i++) {
-		getContact($userCards[i]);
+		writeUserCard(i);
 	}
 }
 
-
+//Ajax request to get new contact data from random user generator api
+$.ajax({
+	url: 'https://randomuser.me/api/?results=12',
+	dataType: 'json',
+	//if successful store relevent data about user
+	success: function(data) {
+		console.log(data);
+		newPerson = data;
+		writeEachCard();			
+	}
+});
 
