9b54348bb7ab8afb6a10c0029a71f36c3cc995b8
Kate Ross <kaeross@icloud.com>
2017-08-10 16:40:05 +0100

    Update 3

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
-function getPersonData(newPerson) {
-	const personResults = newPerson.results[0];
-	const personData = {
-		firstName: personResults.name.first,
-		lastName: personResults.name.last,
-		email: personResults.email,
-		city: personResults.location.city,
-		phone: personResults.phone
-	}
-	return personData;
-}
+const $avatarHTML = $('.avatar');
+const $phoneHTML = $('.phone');
+const $cityHTML = $('.location');
+const $addressHTML = $('.address');
+const $emailHTML = $('.email');
+const $nameHTML = $('.name');
 
-function getNewPerson() {
+
+function Person(name, email, city, phone, avatar, address){
+	this.name = name;
+	this.email = email;
+	this.city = city;
+	this.phone = phone;
+	this.avatar = avatar;
+	this.address = address;
+};
+
+Person.prototype.writeUserCard = function(){
+	$avatarHTML.attr('src', this.avatar);
+	$phoneHTML.html(this.phone);
+	$cityHTML.html(this.city);
+	$addressHTML.html(this.address);
+	$emailHTML.html(this.email);
+	$nameHTML.html(this.name);
+};
+
+function getContact() {
 	$.ajax({
 		url: 'https://randomuser.me/api/',
 		dataType: 'json',
 		success: function(data) {
-			console.log(data);
-			const newPersonData = data;
-			getPersonData(newPersonData);
+			console.log('success');
+			const newPerson = data;
+
+			function getPersonData() {
+				const personResults = newPerson.results[0];
+				const personData = {
+					name: personResults.name.first + ' ' + personResults.name.last,
+					email: personResults.email,
+					city: personResults.location.city,
+					phone: personResults.phone,
+					avatar: personResults.picture.large,
+					address: personResults.location.street + ', ' + personResults.location.postcode
+				}
+				return personData;
+			}
+			var firstPerson = new Person(getPersonData().name, getPersonData().email, getPersonData().city, getPersonData().phone, getPersonData().avatar, getPersonData().address)
+
+			firstPerson.writeUserCard();
 		}
 	});
 }
 
 
 
-function Person(firstName, lastName, email, city, phone){
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.email = email;
-	this.city = city;
-	this.phone = phone;
 
-};
-
-var firstPerson = new Person(getPersonData().firstName, getPersonData().lastName, getPersonData().email, getPersonData().city, getPersonData().phone);
