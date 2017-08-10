c3f130dced860308e58e7bf722a8db1079fdc262
Kate Ross <kaeross@icloud.com>
2017-08-10 17:23:09 +0100

    Update 4

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
-const $avatarHTML = $('.avatar');
-const $phoneHTML = $('.phone');
-const $cityHTML = $('.location');
-const $addressHTML = $('.address');
-const $emailHTML = $('.email');
-const $nameHTML = $('.name');
 
+const $userCards = $('.user');
 
-function Person(name, email, city, phone, avatar, address){
-	this.name = name;
-	this.email = email;
-	this.city = city;
-	this.phone = phone;
-	this.avatar = avatar;
-	this.address = address;
-};
-
-Person.prototype.writeUserCard = function(){
-	$avatarHTML.attr('src', this.avatar);
-	$phoneHTML.html(this.phone);
-	$cityHTML.html(this.city);
-	$addressHTML.html(this.address);
-	$emailHTML.html(this.email);
-	$nameHTML.html(this.name);
-};
-
-function getContact() {
+//Get new contact data from random user generator api
+function getContact(card) {
 	$.ajax({
 		url: 'https://randomuser.me/api/',
 		dataType: 'json',
+		//if successful store relevent data about user
 		success: function(data) {
 			console.log('success');
 			const newPerson = data;
 
+			//function to get data and store in object
 			function getPersonData() {
 				const personResults = newPerson.results[0];
 				const personData = {
@@ -44,13 +24,43 @@ function getContact() {
 				}
 				return personData;
 			}
-			var firstPerson = new Person(getPersonData().name, getPersonData().email, getPersonData().city, getPersonData().phone, getPersonData().avatar, getPersonData().address)
+			//create person
+			function Person(name, email, city, phone, avatar, address){
+				this.name = name;
+				this.email = email;
+				this.city = city;
+				this.phone = phone;
+				this.avatar = avatar;
+				this.address = address;
+			};
+			//write contact to user card
+			Person.prototype.writeUserCard = function(card){
+				const $avatarHTML = $('> .avatar', card);
+				const $phoneHTML = $('> div > .phone', card);
+				const $cityHTML = $('> div > .location', card);
+				const $addressHTML = $('> div > .address', card);
+				const $emailHTML = $('> div > .email', card);
+				const $nameHTML = $('> div > .name', card);
 
-			firstPerson.writeUserCard();
+				$avatarHTML.attr('src', this.avatar);
+				$phoneHTML.html(this.phone);
+				$cityHTML.html(this.city);
+				$addressHTML.html(this.address);
+				$emailHTML.html(this.email);
+				$nameHTML.html(this.name);
+			};
+			//create newPerson and add content to user card
+			var getPerson = new Person(getPersonData().name, getPersonData().email, getPersonData().city, getPersonData().phone, getPersonData().avatar, getPersonData().address)
+			getPerson.writeUserCard(card);
 		}
 	});
 }
 
+function writeEachCard() {
+	for (let i = 0; i < $userCards.length; i++) {
+		getContact($userCards[i]);
+	}
+}
 
 
 
