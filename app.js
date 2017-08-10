bf20b53739e185ae941c4823738ff57fa4ba601b
Kate Ross <kaeross@icloud.com>
2017-08-10 15:40:25 +0100

    Update 2

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-$.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-  }
-});
+function getPersonData(newPerson) {
+	const personResults = newPerson.results[0];
+	const personData = {
+		firstName: personResults.name.first,
+		lastName: personResults.name.last,
+		email: personResults.email,
+		city: personResults.location.city,
+		phone: personResults.phone
+	}
+	return personData;
+}
+
+function getNewPerson() {
+	$.ajax({
+		url: 'https://randomuser.me/api/',
+		dataType: 'json',
+		success: function(data) {
+			console.log(data);
+			const newPersonData = data;
+			getPersonData(newPersonData);
+		}
+	});
+}
+
+
+
+function Person(firstName, lastName, email, city, phone){
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	this.city = city;
+	this.phone = phone;
+
+};
+
+var firstPerson = new Person(getPersonData().firstName, getPersonData().lastName, getPersonData().email, getPersonData().city, getPersonData().phone);
