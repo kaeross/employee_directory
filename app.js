ab048f5d91c63546b2eb2809d11ddd4dc5380c85
Kate Ross <kaeross@icloud.com>
2017-08-12 17:57:14 +0100

    Update 9

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const $userCards = $('.user');
 const $userModal = $('.user-modal');
+var currentUserIndex;
 let newPerson;
-
 $userCards.hide();
 
 /***********************************************************
@@ -17,11 +17,12 @@ function getPersonData(i) {
 		city: personResults.location.city,
 		phone: personResults.phone,
 		avatar: personResults.picture.large,
-		address: personResults.location.street + ', ' + personResults.location.postcode,
+		address: personResults.location.street + ', ' + personResults.location.state + ' ' + personResults.location.postcode,
 		dob: personResults.dob
 	}
 	return personData;
 }
+
 
 //writes each user to each card
 function writeEachCard() {
@@ -45,40 +46,113 @@ function writeEachCard() {
 
 		$userCards.show();
 	}
-
 	//create modal window for user card
 	function createUserModal(num) {
+		
+		const phone = $('> div > .phone', $userModal.eq(num));
 		//get inner HTML of Target
 		const $userModalHTML = $userCards.eq(num).html();
 		//set as HTML of $userModal
 		$userModal.eq(num).html($userModalHTML);
+		//add hr above phone
 	}
-
 	for (let i = 0; i < $userCards.length; i++) {
 		writeUserCard(i);
 		createUserModal(i);
 	}
 }
 
+
+
+/***********************************************************
+	Flick between modal windows
+	***********************************************************/
+// create arrows to select left and right
+
+	//get index of current modal 
+		//right click -> index + 1, left click -> index -1
+
+		//if 
+$userCards.on('click', function() {
+	//var 
+	currentUserIndex = $userCards.index(this);
+	console.log(currentUserIndex);
+});
+
+/***********************************************************
+	Search box and function
+***********************************************************/
+
+//Append search input to header
+function searchBox() {
+	let searchHTML = '<div class="user-search">';
+	searchHTML += '<input placeholder="Search for users">';
+	searchHTML += '</div>';
+
+	return searchHTML;
+}
+$('.page-header').append(searchBox());
+
+
+function notFound() {
+	let notFoundText = '<div class="not-found"><h4>No users match your query... Please try again</h4><button class="not-found-button">See all users</button></div>';
+	return notFoundText;
+}
+
+//Add search function
+function searchList() {
+	//clear matched if searchList has already been called
+	let matched = [];
+	//obtain value of search input
+	const searchInput = $('.user-search > input');
+	let searchInputVal = searchInput.val().toUpperCase();
+
+	//if search has already been performed and no results, remove .not-found so does not show twice
+	if ($('.not-found').is(":visible")) {
+		$('.not-found').remove();
+	}
+
+	// Check if each user name contains search query
+	$userCards.each( function() {
+		var $nameHTML = $('> div > .name', this);
+		if( $nameHTML.text().toUpperCase().indexOf(searchInputVal) > -1 ) {
+			// add this user to list of “matched” student
+			matched.push(this);
+		} 
+	});
+
+	// if not found display message
+	if (matched.length === 0) {
+		$userCards.hide();
+		$('.page-header').append(notFound);
+		const notFoundButton = $('.not-found-button');
+		notFoundButton.on('click', function() {
+			console.log('click click');
+			$userCards.show();
+			$('.not-found').remove();
+		});
+		$('.not-found').css({'margin': '2em', 'text-align': 'center', 'font-size': '1em','padding': '3em 0'});
+		searchInput.val('');
+	} else {
+		//show only matched users
+		$userCards.hide();
+		$(matched).show();
+	}
+}
+
+/***********************************************************
+	Run Ajax request and then call functions
+	***********************************************************/
+	
 //Ajax request to get new contact data from random user generator api
 $.ajax({
-	url: 'https://randomuser.me/api/?results=12',
+	url: 'https://randomuser.me/api/?results=12&nat=gb,us',
 	dataType: 'json',
 	//if successful store relevent data about user
 	success: function(data) {
 		console.log(data);
 		newPerson = data;
-		writeEachCard();			
+		writeEachCard();
+		$('.user-search > input').on('keyup keypress', searchList);			
 	}
 });
-
-/***********************************************************
-	Create user modal window
-	***********************************************************/
-
-
-	//get index of current modal 
-		//right click -> index + 1, left click -> index -1
-
-
-
